212e2389229ccfb63fe2f9f22dad75795c565a4e
almirakrdzic <a.lmira@hotmail.com>
2014-03-14 11:15:32 +0100

    Added contracts for Book and Author

--- DigitalLibrary/DigitalLibraryService/Contracts/Book.cs
+++ DigitalLibrary/DigitalLibraryService/Contracts/Book.cs
@@ -9,18 +9,19 @@ namespace DigitalLibraryService.Contracts
     [DataContract]
     public class Book
     {
+        [DataMember]
         public int Id { get; set; }
-
+        [DataMember]
         public string Title { get; set; }
-
+        [DataMember]  
         public string ISBN { get; set; }
-
+         [DataMember]
         public string Edition { get; set; }
-
+         [DataMember]
         public string Description { get; set; }
-
+         [DataMember]
         public string Content { get; set; }
-
+         [DataMember]
         public User AddedBy { get; set; }
 
 
